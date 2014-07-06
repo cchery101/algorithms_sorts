36363f77df498d0550166da7716a04cad234a97b
Simon Bowly <simon.bowly@gmail.com>
2014-07-06 19:17:23 +1000

    implemented selection sort for generic comparable type

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,42 +1,43 @@
 
 import java.util.Arrays;
 
-public class SelectionSort
+public class SelectionSort<Item>
 {
-    Integer[] data;
-
-    private boolean lessthan(int i, int j) {
-        return data[i] < data[j];
+    private static boolean lessthan(Comparable a, Comparable b) {
+        return a.compareTo(b) < 0;
     }
     
-    private void exchange(int i, int j) {
-        Integer temp = data[i];
+    private static void exchange(Comparable[] data, int i, int j) {
+        Comparable temp = data[i];
         data[i] = data[j];
         data[j] = temp;
     }
     
-    public void sort(Integer[] inputdata)
+    public static void sort(Comparable[] data)
     {
-        // create internal reference to the input array
-        data = inputdata;
         int N = data.length;
         int minval;
         // selection loop
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N - 1; i++)
         {
             minval = i;
             for (int j = i + 1; j < N; j++)
-                { if (lessthan(j, minval)) minval = j; }
-            exchange(i, minval);
+                { if (lessthan(data[j], data[minval])) minval = j; }
+            exchange(data, i, minval);
         }
     }
 
     public static void main(String[] args)
     {
-        Integer[] myarray = new Integer[] {3, 4, 5, 1, 2, 7, 8, 6, 10, 9, 11};
-        SelectionSort sorter = new SelectionSort();
-        System.out.println(Arrays.toString(myarray));
-        sorter.sort(myarray);
-        System.out.println(Arrays.toString(myarray));
+        // test with integers
+        Integer[] myintarray = new Integer[] {3, 4, 5, 1, 2, 7, 8, 6, 10, 9, 11};
+        System.out.println(Arrays.toString(myintarray));
+        SelectionSort.sort(myintarray);
+        System.out.println(Arrays.toString(myintarray));
+        // test with strings
+        String[] mystrarray = new String[] {"C", "B", "F", "A", "D", "E"};
+        System.out.println(Arrays.toString(mystrarray));
+        SelectionSort.sort(mystrarray);
+        System.out.println(Arrays.toString(mystrarray));
     }
 }
