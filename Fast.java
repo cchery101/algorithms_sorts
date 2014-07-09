a41e16ef003de374ed7f5267023cc179567f9cee
Simon Bowly <simon.bowly@gmail.com>
2014-07-09 22:32:19 +1000

    line storage and printing class

--- Fast.java
+++ Fast.java
@@ -1,14 +1,62 @@
 
 import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.Iterator;
 
 public class Fast {
-    private static void getlines(Point[] points) {
+    private LinkedQueue<Line> lines = new LinkedQueue<Line>();
+
+    private class Line {
+        private LinkedQueue<Point> points = new LinkedQueue<Point>();
+        private Point startpoint;
+        private Point endpoint;
+        public Line(Point pt1, Point pt2, Point pt3) {
+            startpoint = pt1;
+            points.enqueue(pt1);
+            points.enqueue(pt2);
+            points.enqueue(pt3);
+            endpoint = pt3;
+        }
+        public void addpoint(Point pt) {
+            points.enqueue(pt);
+            endpoint = pt;
+        }
+        public void printline() {
+            System.out.print(points.dequeue().toString());
+            for (Point pt: points) {
+                System.out.print(" -> ");
+                System.out.print(pt.toString());
+            }
+            System.out.println();
+        }
+        public boolean hassubset(Line that) {
+            Iterator<Point> thisiter = points.iterator();
+            thisiter.next();
+            Iterator<Point> thatiter = that.points.iterator();
+            try {
+                while (true) {
+                    if (thisiter.next().compareTo(thatiter.next()) != 0) {
+                        // mismatch, so not a subset
+                        return false;
+                    }
+                }
+            }
+            catch (NoSuchElementException e) {
+                // reached end of list without a mismatch
+                return true;
+            }
+        }
+    }
+
+    private void getlines(Point[] points) {
         int N = points.length;
         Point[] aux;
         Point start;
         double currentslope;
         double slope;
         boolean online;
+        int linestart;
+        Line currentline = new Line(points[0], points[1], points[2]);
         // sort points by increasing y coordinate
         Arrays.sort(points);
         for (int i = 0; i < N - 2; i++) {
@@ -24,27 +72,38 @@ public class Fast {
             for (int j = 1; j < N - i - 1; j++) {
                 slope = start.slopeTo(aux[j]);
                 if (slope == currentslope) {
-                    if (online)
+                    if (online) {
                         // already on a line, found an extra point
-                        { System.out.print(String.format(" --> %s", aux[j].toString())); }
+                        System.out.print(String.format(" --> %s", aux[j].toString()));
+                        currentline.addpoint(aux[j]);
+                        }
                     else {
                     // found 3 collinear points to start a line
                         System.out.print(String.format("%s --> %s --> %s",
                             start.toString(), aux[j-1].toString(), aux[j].toString()));
                         online = true;
+                        currentline = new Line(start, aux[j-1], aux[j]);
                     }
                 }
                 else {
-                    if (online) System.out.println();
+                    if (online) {
+                        // finish the current line
+                        lines.enqueue(currentline);
+                        System.out.println();
+                    }
                     online = false;
                 }
                 currentslope = slope;
             }
-            if (online) System.out.println();
+            if (online) {
+                lines.enqueue(currentline);
+                System.out.println();
+            }
         }
     }
 
     public static void main(String[] args) {
+        Fast testobj = new Fast();
         // read in the input file
         In in = new In(args[0]);      // input file
         int N = in.readInt();         // N points in the set
@@ -57,6 +116,18 @@ public class Fast {
             points[idx] = new Point(i, j);
             idx++;
         }
-        getlines(points);
+        // get lines
+        System.out.println("Getting Lines");
+        testobj.getlines(points);
+        // print lines
+        System.out.println("Printing Lines");
+        for (Line l: testobj.lines) {
+            for (Line parent: testobj.lines) {
+                if (parent.hassubset(l)) {
+                    System.out.print("Dupl  ");
+                }
+            }
+            l.printline();
+        }
     }
 }
