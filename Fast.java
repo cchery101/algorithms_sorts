a7f0dc2b08dea9e1d90cd3b69947236e39fe1deb
Simon Bowly <simon.bowly@gmail.com>
2014-07-09 21:50:42 +1000

    prints full length lines, but still has subsets

--- Fast.java
+++ Fast.java
@@ -8,6 +8,7 @@ public class Fast {
         Point start;
         double currentslope;
         double slope;
+        boolean online;
         // sort points by increasing y coordinate
         Arrays.sort(points);
         for (int i = 0; i < N - 2; i++) {
@@ -19,15 +20,27 @@ public class Fast {
             // sort remaining points by slope to the current start
             Arrays.sort(aux, start.SLOPE_ORDER);
             currentslope = start.slopeTo(aux[0]);
+            online = false;
             for (int j = 1; j < N - i - 1; j++) {
                 slope = start.slopeTo(aux[j]);
                 if (slope == currentslope) {
-                    // found 3 collinear points
-                    System.out.println(String.format("%s --> %s --> %s",
-                        start.toString(), aux[j-1].toString(), aux[j].toString()));
+                    if (online)
+                        // already on a line, found an extra point
+                        { System.out.print(String.format(" --> %s", aux[j].toString())); }
+                    else {
+                    // found 3 collinear points to start a line
+                        System.out.print(String.format("%s --> %s --> %s",
+                            start.toString(), aux[j-1].toString(), aux[j].toString()));
+                        online = true;
+                    }
+                }
+                else {
+                    if (online) System.out.println();
+                    online = false;
                 }
                 currentslope = slope;
             }
+            if (online) System.out.println();
         }
     }
 
